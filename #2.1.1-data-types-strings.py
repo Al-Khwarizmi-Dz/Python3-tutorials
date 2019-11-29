6ec16aea5518da3052b5e293bd5eef0202b800b7
Herhar Fares <58251896+1001Programing@users.noreply.github.com>
2019-11-29 17:21:49 +0100

    good

---
+++ #2.1.1-data-types-strings.py
@@ -0,0 +1,34 @@
+'''
+print, est une fonction pour afficher
+print, is a function to display on the screen
+print, هي فنكشن للعرض
+'''
+
+# declaration de la variable string
+# declaration of the string variable with double quotation marks
+string = "Fares is here."
+print(string)
+
+# declaration de la variable string1
+# declaration of the string1 variable with double quotation marks
+string1 = 'Fares is here.'
+print(string1)
+
+# in case we use the single quotation mark, in between
+# a single quotaion string, we get an error.
+# like this one:: 'I need help, i can't.'
+shit = "I need help, i can't."
+print(shit)
+
+# in case we want to have a multi line python string
+# we use """, instad of "
+# or
+string3 = """uhsdbfiugbsdiqufgsdqf
+qsdfqsfqsfqsfsqdfsqfsqfsqfdsqfqs"""
+print(string3)
+
+# we use the ''', instad of '''
+# don't worry of the usage, we'll get to that
+string4 = '''uhsdbfiugbsdiqufgsdqf
+qsdfqsfqsfqsfsqdfsqfsqfsqfdsqfqs'''
+print(string4)
