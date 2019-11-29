afdca627c4b01c43e0e96be653678aec8befd5c1
Herhar Fares <58251896+1001Programing@users.noreply.github.com>
2019-11-29 15:47:14 +0100

    First push

---
+++ #2.0-data-types.py
@@ -0,0 +1,37 @@
+# strings, chaine de caractere
+s = "Hello"
+print(s)
+
+# int, entier
+a = 1
+# float, reele
+b = 1.2
+# complex
+c = 1 + 1j
+# hexadecimal
+d = 0xFF
+# boolean
+e = False
+ee = True
+print(a, b, c, d)
+print(e, ee)
+
+# list, tableau
+lol = [1, 2, 3, 3]
+print(lol, lol[0])
+
+# set
+# list, mais sans les doublons
+# without duplicates
+set_ = {1, 2, 3, 3}
+print(set_)
+
+# tuple
+# list, mais no modifiable, no mutable
+# not mutable list
+tuple_ = (1, 2, 3, 3)
+print(tuple_)
+
+# dictionary, dictionaire
+dic = {"one": 1, "two": 2, "DZ": 213}
+print(dic, dic["DZ"])
