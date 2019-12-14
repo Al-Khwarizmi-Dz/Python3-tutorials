7581d237679023315408fb78cd876572c8fce0d5
Herhar Fares <58251896+1001Programing@users.noreply.github.com>
2019-12-14 13:19:52 +0100

    Sat Dec 14 13:19:52 CET 2019

---
+++ #2.2-data-types-numbers.py
@@ -0,0 +1,33 @@
+num = 13
+num1 = -12
+print(num, num1)
+
+a = 0b111110
+print(a)
+
+b = 0o12345670
+print(b)
+
+c = 0xff234560a
+print(c)
+
+d = 0x111110
+print(d)
+
+
+num_float = 12.3
+num_float1 = -12.8
+print(num_float, num_float1)
+
+
+num_comlex = -1 + 12.5j
+print(num_comlex)
+
+
+s = 10e2
+print(s)
+
+
+true_bool = True
+false_bool = False
+print(true_bool, false_bool)
