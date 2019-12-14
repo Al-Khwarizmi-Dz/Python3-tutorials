002f8f2f9c6f51fb7b2b1c080d87f7a9b5ac1031
Herhar Fares <58251896+1001Programing@users.noreply.github.com>
2019-12-14 13:23:37 +0100

    Sat Dec 14 13:23:37 CET 2019

--- #2.2-data-types-numbers.py
+++ #2.2-data-types-numbers.py
@@ -1,33 +1,38 @@
+# entiers
 num = 13
 num1 = -12
 print(num, num1)
 
+# binaire
 a = 0b111110
 print(a)
 
+# octal
 b = 0o12345670
 print(b)
 
+hexadecimal
 c = 0xff234560a
 print(c)
 
+# binaire et hexadecimal, pas la meme valeur
 d = 0x111110
 print(d)
 
-
+# reele
 num_float = 12.3
 num_float1 = -12.8
 print(num_float, num_float1)
 
-
+# complex
 num_comlex = -1 + 12.5j
 print(num_comlex)
 
-
+# power, 10 * 10**2 = 10 * 10 *10 = 1000
 s = 10e2
 print(s)
 
-
+# boolean, vrai, faux
 true_bool = True
 false_bool = False
 print(true_bool, false_bool)
