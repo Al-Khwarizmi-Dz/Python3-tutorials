272cf67dcb069cd40a2e17282c7d782eeb4e044a
Herhar Fares <herhar.fares@yandex.com>
2020-03-31 04:00:39 +0100

    Tue 31 Mar 04:00:39 CET 2020

--- #2.3-data-types-lists.py
+++ #2.3-data-types-lists.py
@@ -10,73 +10,74 @@ lol = list()
 ll = []
 
 # brackets, with elements
-# as you can see, The -lll- list holds many objects of
+# as you can see, The -lol- list holds many objects of
 # different types.
-lll = [1, "string", [12], False, 1.23, (1 + 1j), True]
+lol = [1, "string", [12], False, 1.23, (1 + 1j), True]
 
 ''' Lenght of a list. '''
 # getting the lenght of the list
-# len(lll) = 7
-lenght = len(lll)
+# len(lol) = 7
+lenght = len(lol)
 
 ''' Accessing elemnts. '''
+# index = position - 1
 # first element
-out = lll[0]
+out = lol[0]
 
 # second element
-out = lll[1]
+out = lol[1]
 
 # last element
-out = lll[-1]
-out = lll[len(lll) - 1]
+out = lol[-1]
+out = lol[len(lol) - 1]
 
 # before the last element
-out = lll[-2]
-out = lll[len(lll) - 2]
+out = lol[-2]
+out = lol[len(lol) - 2]
 
 ''' Mutability notion'''
 # adding the refence
-temp = lll
+temp = lol
 
-# adding a new element to temp, that is the same lll
+# adding a new element to temp, that is the same lol
 temp.append(1997)
 
 ''' Slicing elemnts. '''
 # copying the all the list
-test = lll[:]  # method one
-test = lll[0:]  # method two
-test = lll[:len(lll)]  # method three
-test = lll[0:len(lll)]  # method four
+test = lol[:]  # method one
+test = lol[0:]  # method two
+test = lol[:len(lol)]  # method three
+test = lol[0:len(lol)]  # method four
 
 # copying the all the list, except the last element
 # return to the [2.1.2-data-types-strings.py] file
-test = lll[:-1]  # method one
-test = lll[:len(lll) - 1]  # method two
+test = lol[:-1]  # method one
+test = lol[:len(lol) - 1]  # method two
 
 # copying the list from, the first element to the third
-# len(lll) = 7
+# len(lol) = 7
 # i = 0, becasue a have the index 0
 # j = i + 3, j = 0 + 3, j = 3
 # use [0:3]
-test = lll[0:3]  # method one
-test = lll[:3]  # method two
+test = lol[0:3]  # method one
+test = lol[:3]  # method two
 
 # copying the list from, the third element to the fifth
-# len(lll) = 7
+# len(lol) = 7
 # i = 2, becasue a have the index 2
-# j = i + 3, j = 2 + 3, j = 5
+# j = 5, because it is the fifth element
 # use [2:5]
-test = lll[2:5]
+test = lol[2:5]
 
 # copying the all the list, but the step is by 2
 # so we'll be taking the index:: 0, 2, 4, 6 <= 6
 # it can be any thing
-test = lll[::2]
+test = lol[::2]
 
 # copying the all the list, but the step is by 3
 # so we'll be taking the index:: 0, 3, 6 <= 6
 # it can be any thing
-test = lll[::3]
+test = lol[::3]
 
 # printing the string in reverse
 # here the step is equal to -1
@@ -84,27 +85,27 @@ test = lll[::3]
 # in our our case it is form index 0, to index 6
 # this means taking the following
 # 6, 5, 4, 3, 2, 1, 0 <= 0
-temp = lll[l::-1]
+test = lol[len(lol)::-1]
 
 ''' Some functions with the lists'''
 # add a new elements
-lll.append("New Element")
-lll.append(123)
+lol.append("New Element")
+lol.append(123)
 
 # delete an element
-lll.remove(123)
+lol.remove(123)
 
 # get the index of an element
-index = lll.index()
+index = lol.index()
 
 # indert a new element, at a spesific index
-lll.insert("Element", 3)
+lol.insert("Element", 3)
 
 # it returns  the last element of the list, and delets it
-last = lll.pop()
+last = lol.pop()
 
 # empty the list, delete all the elements
-lll.clear()
+lol.clear()
 
 ''' Extra '''
 one = [1, 2, 3]
--- __init__.py
+++ __init__.py
@@ -1,6 +1,1 @@
-Dict = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
-
-temp = Dict
-
-
-print(Dict.items())
+lol = [1, "string", [12], False, 1.23, (1 + 1j), True]
